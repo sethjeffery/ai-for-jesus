a61058478bd6aadf6ab116f7bd59a1c128b8f086
Seth Jeffery <seth.jeffery@varsitytutors.com>
2023-05-27 14:27:02 +0200

    use h2

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,9 +3,9 @@ import { CMS_NAME, CMS_URL } from "../lib/constants";
 export default function Intro() {
   return (
     <section className="md:flex-row md:flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h2 className="text-4xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         Latest Post.
-      </h1>
+      </h2>
     </section>
   );
 }
